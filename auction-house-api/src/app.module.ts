965ed0bb215011e6543e0849eb184f5e16c879dc
aOurfali <a.ourfali@icloud.com>
2023-03-09 09:03:45 +0100

    Database and docker configuration

--- auction-house-api/src/app.module.ts
+++ auction-house-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'test',
+      database: 'auction-house',
+      entities: [],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
